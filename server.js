6f0f69f3213229c28e7533cf4988aa65ff5c679a
Remijius Brian <69680701+RemijiusBrian@users.noreply.github.com>
2022-11-01 07:41:39 +0000

    Refactoring

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ server.all("/", (req, res) => {
 	res.send("Bot is running...");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env['PORT'] || 3000;
 module.exports = function startServer() {
 	server.listen(port, () => {
 		console.log("Server has started!");
