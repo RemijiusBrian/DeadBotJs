9a9c7a6cbc1ddf62f0d46cb9d4d4fc4a023cd151
Remijius Brian <ridilldev@gmail.com>
2022-11-05 01:36:04 +0530

    Added Start/Stop Mc server

--- commands/stop-mc.js
+++ commands/stop-mc.js
@@ -2,16 +2,16 @@ const { SlashCommandBuilder } = require("discord.js");
 const mcUtils = require('../utils/mc-utils');
 const exec = require('child_process').exec;
 
-const START_MC_COMMAND = 'gcloud compute instances start --zone \"asia-south2-a\" \"supercomputer\"  --project \"verdant-algebra-364611\"';
+const STOP_MC_COMMAND = 'gcloud compute instances stop --zone \"asia-south2-a\" \"supercomputer\"  --project \"verdant-algebra-364611\"';
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('start-mc')
-        .setDescription('Starts your Minecraft server'),
+        .setName('stop-mc')
+        .setDescription('Stops your Minecraft server'),
     async execute(interaction) {
         await interaction.deferReply();
         exec(START_MC_COMMAND, async (err, stdout, stderr) => {
-            await interaction.editReply('Starting your Minecraft server...');
+            await interaction.editReply('Stopping your Minecraft server...');
         });
     }
 }
