676c2aaa11134f7b5ed0dfdddea6d1ebcbc464a0
Remijius Brian <ridilldev@gmail.com>
2022-11-05 01:07:35 +0530

    Added ban event

---
+++ commands/timeout-member.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const KEY_MEMBER = 'member';
+const KEY_TIME = 'time';
+const KEY_REASON = 'reason';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('timeout')
+        .setDescription('Timeout a member')
+        .addUserOption(option =>
+            option.setName(KEY_MEMBER)
+                .setDescription('Timeout target')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName(KEY_TIME)
+                .setDescription('Mins to timeout')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName(KEY_REASON)
+                .setDescription('Reason for Timeout')
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const member = interaction.options.getUser(KEY_MEMBER);
+        const time = interaction.options.getInteger(KEY_TIME);
+        const reason = interaction.options.getString(KEY_REASON) ?? 'Reasons Unknown';
+
+        await member.timeout(time * 60 * 1000, reason);
+        await interaction.reply(`${member.tag} is on a timeout for ${reason}`);
+    }
+}
